67a731dc65c7790f47123d6d1b765ce923f46074
Tshepang Masia <100064063+TSH3PH4NG@users.noreply.github.com>
2025-02-23 21:01:10 +0200

    Update and rename index(1).ts to index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const PORT = 8000 || 3000;
+const PORT = 8000
 
 
 async function getBuffer(url, options) {
