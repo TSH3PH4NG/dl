60dd3b26c43ea1e8b29e7e7b01f97015f0293e40
Tshepang Masia <100064063+TSH3PH4NG@users.noreply.github.com>
2025-03-03 23:14:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/download', async (req, res) => {
     
     try {
         const data = await savetube.download(url,videoQuality);
-        return res.send((await fetch(data.result.download)));
+        return res.send(data);
         const videoStream = await axios({
         url: data.result.download,
         method: 'GET',
